b658c806781d87165b72eb12992c2bb54970f0f0
nganlinh4 <nganlinh4@gmail.com>
2025-09-02 15:33:16 +0900

    stop tracking node_modules; keep tracking .env files

--- server/node_modules/@libp2p/webrtc/src/webrtc/index.ts
+++
@@ -1,2 +0,0 @@
-export { RTCSessionDescription, RTCIceCandidate, RTCPeerConnection } from '@ipshipyard/node-datachannel/polyfill'
-export type { RTCDataChannel, RTCDataChannelEvent } from '@ipshipyard/node-datachannel/polyfill'
